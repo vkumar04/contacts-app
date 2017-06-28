1cc13020b76a96516b12368817d60fed31f4019c
vkumar04 <vick.kumar44@gmail.com>
2017-06-28 13:52:44 -0400

    list contacts is now stateless

--- src/ListContacts.jsx
+++ src/ListContacts.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListContacts extends Component {
-  render() {
-    return (
+const ListContacts = (props) => {
+      return (
       <ol className='contact-list'>
-        {this.props.contacts.map((contact) =>
+        {props.contacts.map((contact) =>
           <li key={contact.id} className='contact-list-item'>
            <div className='contact-avatar' style={{
              backgroundImage:`url(${contact.avatarURL})`
@@ -13,11 +12,11 @@ class ListContacts extends Component {
              <p>{contact.name}</p>
              <p>{contact.email}</p>
            </div>
+           <button className='contact-remove'>Remove</button>
           </li>
         )}
       </ol>
     )
-  }
 }
 
 export default ListContacts;

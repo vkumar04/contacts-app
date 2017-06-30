2fa706604da7abbf784dd5b51d5794a7ca750194
vkumar04 <vick.kumar44@gmail.com>
2017-06-29 21:56:54 -0400

    added state to list contacts

--- src/ListContacts.jsx
+++ src/ListContacts.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ListContacts = (props) => {
-      return (
-      <ol className='contact-list'>
-        {props.contacts.map((contact) =>
+class ListContacts extends Component {
+  static PropTypes = {
+    contacts: PropTypes.array.isRequired,
+    removeContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  render() {
+    return (
+     <div className='list-contacts'>
+       <div className='list-contacts-top'>
+        <input type='text' className='search-contacts'/>
+       </div>
+       <ol className='contact-list'>
+        {this.props.contacts.map((contact) =>
           <li key={contact.id} className='contact-list-item'>
-           <div className='contact-avatar' style={{
-             backgroundImage:`url(${contact.avatarURL})`
-           }}/>
-           <div className='contact-details'>
-             <p>{contact.name}</p>
-             <p>{contact.email}</p>
-           </div>
-           <button className='contact-remove' onClick={() => props.removeContact(contact)}>Remove</button>
+            <div className='contact-avatar' style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }} />
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className='contact-remove' onClick={() => this.props.removeContact(contact)}>Remove</button>
           </li>
         )}
       </ol>
+     </div> 
     )
-}
-
-ListContacts.PropTypes = {
-  contacts: PropTypes.array.isRequired,
-  removeContact: PropTypes.func.isRequired
+  }
 }
 
 export default ListContacts;
